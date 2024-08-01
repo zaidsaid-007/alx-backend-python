84902a465d069e51ae54440835eb84bae8e5f458
Zaidsaid-007 <zaidsaid007@gmail.com>
2024-08-01 06:56:51 +0300

    floor annotation

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,6 @@
+#!usr/bin/env python3
+"""This module contains a function that takes a float n as argument and returns the floor of the float."""
+
+def floor(n: float) -> int:
+    """This function takes a float n as argument and returns the floor of the float."""
+    return int(n)
---
+++ 0x00-python_variable_annotations/2-main.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+"""This module contains a function that takes a float n as argument and returns the floor of the float."""
+floor = __import__('2-floor').floor
+print(floor(3.14))
+print(floor.__annotations__)
